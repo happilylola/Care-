16d9dc8f8c93c16d306526bff280b1d5c91e2753
Alaa <alaaalmahdi1999@gmail.com>
2022-03-20 13:59:05 +0200

    about

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,26 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/signup', function () {
+    return view('signup');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/departments', function () {
+    return view('departments');
+});
+
+
+Route::get('/about', function () {
+    return view('about');
 });
